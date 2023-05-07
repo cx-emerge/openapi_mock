050d5df542d33f88c4b69635e3cd570fed639c3c
高翔 <263027768@qq.com>
2023-05-07 15:51:01 +0800

    :sparkles: feat: 添加 必要依赖库

---
+++ src/bootloader.rs
@@ -0,0 +1,10 @@
+use std::convert::Infallible;
+
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::{Request, Response};
+
+
+pub async fn bootloader(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::net::SocketAddr;
+
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use tokio::net::TcpListener;
+
+mod bootloader;
+use bootloader::*;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+	// 监听地址
+	let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+	// 绑定监听地址
+	let listener = TcpListener::bind(addr).await?;
+
+	// 开始阻塞等待连接
+	loop {
+		let (stream, _) = listener.accept().await?;
+
+		tokio::task::spawn(async move {
+			if let Err(err) = http1::Builder::new()
+				.serve_connection(stream, service_fn(bootloader))
+					.await
+			{
+				println!("Error serving connection: {:?}", err);
+			}
+		});
+	}
+}
